b717bce44058a7063cf46ab70153f330706a8ae0
Mr Kushal Jamarkattel <kattelkushal3@gmail.com>
2026-09-17 21:20:29 +0545

    ui enhancement

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     port: 5173,
     open: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", "https://kamkxabackend.vercel.app/",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
